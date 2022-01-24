453aa28b888adca91e3a6be18e13be089b6a1110
radek-drw <rdrweski@gmail.com>
2022-01-24 20:26:22 +0000

    Problem commented for future resolving

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 import Product from '../components/Product';
 
 const ProductPage = (props) => {
-  console.log(props);
+  // TO BE FIXED or will found another way to make it work
+  // console.log(props);
   return (
     <>
       <h2>Strona produktu</h2>
